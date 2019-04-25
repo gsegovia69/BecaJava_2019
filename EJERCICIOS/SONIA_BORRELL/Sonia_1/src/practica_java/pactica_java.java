a2c397bd6131f4ee5a6840554250c682c7ae87d4
Sonia <s.borrell.p@gmail.com>
2019-04-25 16:45:27 +0200

    Update pactica_java.java

--- EJERCICIOS/SONIA_BORRELL/Sonia_1/src/practica_java/pactica_java.java
+++ EJERCICIOS/SONIA_BORRELL/Sonia_1/src/practica_java/pactica_java.java
@@ -5,31 +5,53 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class pactica_java {
+
 	public static void main(String[] args) {
+
 		File directorio = new File("C:\\Users\\Administrador\\Documents\\GitHub\\BecaJava_2019\\EJERCICIOS");
-		String linea;
+		String linea = null;
+		ArrayList<String> lista_a = new ArrayList<String>();
+		HashMap <Integer, String> mapa = new HashMap<Integer, String>();
 		if (directorio.isDirectory()) {
 			File[] lista = directorio.listFiles();
 			for (File fichero : lista) {
 				if (fichero.getName().equals("alumnos.xml")) {
 					try {
-						FileReader archivo = new FileReader(args[0] + "/" + fichero.getName());
+						FileReader archivo = new FileReader(directorio + "/" + fichero.getName());
 						BufferedReader b = new BufferedReader(archivo);
-						while (b.readLine() != null) {
-							linea = b.readLine();
-							System.out.println(linea);
+						int contador = 0;
+						while ((linea = b.readLine()) != null){
+						
+							// linea = b.readLine();
+							//System.out.println(linea);
+
+							lista_a.add(linea);
+							mapa.put (contador++, linea);
 						}
+						
+						System.out.println(lista_a);
+						System.out.println("HOLA, SOY EL MAPA: "+mapa);
+
 						b.close();
-					} catch (FileNotFoundException e) {
+						
+						}	catch (FileNotFoundException e) {
+
 						e.printStackTrace();
+
 						System.out.println(e.toString());
+						
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 				}
+
 			}
 		}
 	}
-}
+	
+	}
+
