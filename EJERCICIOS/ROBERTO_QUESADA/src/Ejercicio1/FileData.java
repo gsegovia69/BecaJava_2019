423ba2b363fa013b7f753241c827164a25093c48
Roberto Quesada <robertocqo@gmail.com>
2019-04-25 11:32:50 +0200

    Update FileData.java
    changed file path

--- EJERCICIOS/ROBERTO_QUESADA/src/Ejercicio1/FileData.java
+++ EJERCICIOS/ROBERTO_QUESADA/src/Ejercicio1/FileData.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class FileData {
 
 	public static void main(String[ ] args) throws IOException {
-		String file_name = "C:/Users/Administrador/Documents/GitHub/BecaJavatortoise/EJERCICIOS/alumnos.xml";
+		String file_name = "../alumnos.xml";
 		try {
 			ReadFile file = new ReadFile( file_name );
 			String[ ] aryLines = file.OpenFile( );
