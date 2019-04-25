dc203a243d682387d68540e543f1215b15c4db14
Roberto Quesada <robertocqo@gmail.com>
2019-04-25 16:03:29 +0200

    Update Lectorv2.java

--- EJERCICIOS/ROBERTO_QUESADA/src/Ejercicio1/Lectorv2.java
+++ EJERCICIOS/ROBERTO_QUESADA/src/Ejercicio1/Lectorv2.java
@@ -1,18 +1,13 @@
 package Ejercicio1;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Lectorv2 {
 
 	public static void main(String[] args) throws IOException {
-		FileReader fr = new FileReader("../alumnos.xml");
-		BufferedReader bf = new BufferedReader(fr);
-		Files.lines(Paths.get("../alumnos.xml")).forEach(System.out::println);
-		bf.close();
+		Files.lines(Paths.get("../alumnos.xml"),StandardCharsets.ISO_8859_1).forEach(System.out::println);
 	}
 	
 }
