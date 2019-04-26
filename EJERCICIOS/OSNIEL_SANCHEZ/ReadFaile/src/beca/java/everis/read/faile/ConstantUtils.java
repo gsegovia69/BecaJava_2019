449cb44237b46bf6f3b9ddbe15892d4e90256f0e
osniel <onmymusica@gmail.com>
2019-04-26 12:10:53 +0200

    1.4

---
+++ EJERCICIOS/OSNIEL_SANCHEZ/ReadFaile/src/beca/java/everis/read/faile/ConstantUtils.java
@@ -0,0 +1,14 @@
+package beca.java.everis.read.faile;
+
+public class ConstantUtils {
+	
+	public static final String INICIO_ITERACION_NOMBRE = "<nombre>";
+	public static final String FIN_ITERACION_NOMBRE = "</nombre>";
+	public static final String INICIO_ITERACION_APELLIDOS = "<apellidos>";
+	public static final String FIN_ITERACION_APELLIDOS = "</apellidos>";
+	public static final String INICIO_ITERACION_CIUDAD = "<ciudad>";
+	public static final String FIN_ITERACION_IUDAD = "</ciudad>";
+	public static final String INICIO_ITERACION_EMAIL = "<email>";
+	public static final String FIN_ITERACION_EMAIL = "</email>";
+
+}
