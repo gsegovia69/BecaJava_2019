0b4e0548a0dda3205b761dafd2a941e7e98de852
Alejandro Bueno <alejandro.bueno.alcazar@gmail.com>
2019-04-29 13:37:14 +0200

    1

---
+++ EJERCICIOS/ALEJANDRO_BUENO/Beca2/src/Beca2/Constantes.java
@@ -0,0 +1,17 @@
+package Beca2;
+
+public class Constantes
+{
+	public static final String INICIO_ITERACION_PROFESOR = "<profesor>";
+	public static final String FIN_ITERACION_PROFESOR = "</profesor>";
+	public static final String INICIO_ITERACION_ALUMNO = "<alumno>";
+	public static final String FIN_ITERACION_ALUMNO = "</alumno>";
+	public static final String INICIO_ITERACION_NOMBRE = "<nombre>";
+	public static final String FIN_ITERACION_NOMBRE = "</nombre>";
+	public static final String INICIO_ITERACION_APELLIDOS = "<apellidos>";
+	public static final String FIN_ITERACION_APELLIDOS = "</apellidos>";
+	public static final String INICIO_ITERACION_EMAIL = "<email>";
+	public static final String FIN_ITERACION_EMAIL = "</email>";
+	public static final String INICIO_ITERACION_CIUDAD = "<ciudad>";
+	public static final String FIN_ITERACION_CIUDAD = "</ciudad>";
+}
