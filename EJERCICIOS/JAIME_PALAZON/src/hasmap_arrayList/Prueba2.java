5a56411a9c43b15ac273705c5a9124c78294b906
Jaime <jpg9_1hotmail.com>
2019-04-29 15:44:59 +0200

    Update Prueba2.java

--- EJERCICIOS/JAIME_PALAZON/src/hasmap_arrayList/Prueba2.java
+++ EJERCICIOS/JAIME_PALAZON/src/hasmap_arrayList/Prueba2.java
@@ -3,19 +3,20 @@ package jaimepalazon;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Prueba2 {
 
 	public static void main(String[] args) {
+		
 		try {
 			FileReader fr = new FileReader("C:/Users/Administrador/Desktop/Ejercicio4_XML.xml");
 			BufferedReader br = new BufferedReader(fr);
 
 			String linea = "";
-			HashMap<Integer, String> xmlDatos = new HashMap<Integer, String>();
+			Map<Integer, String> xmlDatos = new HashMap<Integer, String>();
 			AtomicInteger count = new AtomicInteger(1);
 			while (linea != null) {
 				linea = br.readLine();
