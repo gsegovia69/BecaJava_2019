24c436d79ed2e80f54fb7e97bd6d89c5d9dc3849
Ivan <isrmail87@gmail.com>
2019-05-03 15:04:31 +0200

    Connection All ok

--- EJERCICIOS/IVAN_SANCHEZ/conexionBaseDatos/src/main/java/com/everis/beca/java/conexionBaseDatos/App.java
+++ EJERCICIOS/IVAN_SANCHEZ/conexionBaseDatos/src/main/java/com/everis/beca/java/conexionBaseDatos/App.java
@@ -1,13 +1,36 @@
 package com.everis.beca.java.conexionBaseDatos;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       try {
+		GetConection connectionDatabase = new GetConection();
+		Connection conn = connectionDatabase.getConnection();
+		Statement st = conn.createStatement();
+//		String queryInsert = "insert into alumno (nombre, apellidos, email, ciudad, id_clase) values ('Iván', 'Sánchez Rico','isrmail87@gmail.com','Elda', 1)";
+//		st.executeUpdate(queryInsert);
+		ResultSet rs =null;
+		String querySelect = "Select * from alumno";
+		st = conn.createStatement();
+		rs = st.executeQuery(querySelect);
+		System.out.println("Alumnos");
+		while(rs.next()) {
+			String id = rs.getString(1);
+			String nombre = rs.getString(2);
+			System.out.println(id + " " + nombre);
+		}
+		st.close();
+		rs.close();
+		conn.close();
+	
+		
+	} catch (Exception e) {
+		// TODO: handle exception
+	} 
     }
 }
--- EJERCICIOS/IVAN_SANCHEZ/conexionBaseDatos/src/main/java/com/everis/beca/java/conexionBaseDatos/GetConection.java
+++ EJERCICIOS/IVAN_SANCHEZ/conexionBaseDatos/src/main/java/com/everis/beca/java/conexionBaseDatos/GetConection.java
@@ -6,26 +6,35 @@ import java.sql.SQLException;
 
 public class GetConection {
 
-	
-	public Connection getConnection() throws SQLException{
-	
-		String url= "";
-		
+	private static final String URL_DB = "jdbc:mysql://10.118.124.196:3306/beca_java?useTimezone=true&serverTimezone=UTC";
+	private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
+	private static final String USER = "beca_java";
+	private static final String PWD = "beca_java";
+
+	public Connection getConnection() {
+Connection conn = null;
 		try {
 //		Carga del driver jdbc
-			Class.forName("");
-//		Conexion con la base de datos.
-			
-			
-			
-			
-	} catch (ClassNotFoundException e) {
-		System.out.println("Error al conectar con el driver JDBC");
-	}	
-		Connection cn = DriverManager.getConnection(url);
+			Class.forName(DRIVER_CLASS).newInstance();
+//			Conexion con la base de datos.
+			conn = DriverManager.getConnection(URL_DB,USER,PWD);
+
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error al conectar con el driver JDBC");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		return cn;
+
+		return conn;
 	}
-	
-	
+
 }
