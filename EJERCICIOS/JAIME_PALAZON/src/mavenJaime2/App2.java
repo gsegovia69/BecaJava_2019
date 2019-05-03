c64efaed9ce2ee42024073b8de0a9cb99dc49153
Jaime <jpg9_1hotmail.com>
2019-05-03 12:30:47 +0200

    Create App2.java

---
+++ EJERCICIOS/JAIME_PALAZON/src/mavenJaime2/App2.java
@@ -0,0 +1,26 @@
+package com.everis.jaime.mavenJaime2;
+
+
+
+
+
+public class App2 {
+	
+	public static void main( String[] args ) {
+		
+	try {
+		ListadoAlumnosA a = new ListadoAlumnosA();
+		 
+		 a.obtenerClase2();
+		 
+	} catch (Exception e) {
+		
+	}
+}
+	
+	
+}
+	
+	
+	
+
