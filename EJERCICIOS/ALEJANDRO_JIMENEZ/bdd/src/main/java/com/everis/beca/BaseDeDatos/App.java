24536a09ecd0524e188bc592f7835f0596330d5f
jimenawer <alejandrojimenezerades@gmail.com>
2019-05-03 14:49:05 +0200

    Add BBDD cosas

---
+++ EJERCICIOS/ALEJANDRO_JIMENEZ/bdd/src/main/java/com/everis/beca/BaseDeDatos/App.java
@@ -0,0 +1,53 @@
+package com.everis.beca.BaseDeDatos;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try {
+		ConexionesBD connectionDatabase = new ConexionesBD();
+		Connection conn = connectionDatabase.getConnection();
+		Statement st = conn.createStatement();
+		//String queryInsert= "insert into alumno(nombre,apellidos,email,ciudad,id_clase) values ('Alejandro','Jimenez Erades','alejandrojimenezerades@gmail.com','Aspe',1)";
+		//st.executeUpdate(queryInsert);
+		ResultSet rs = null;
+		String QuerySelect = "Select * from profesor";
+		st = conn.createStatement();
+		rs = st.executeQuery(QuerySelect);
+		System.out.println("Profesor");
+		while (rs.next()) {
+			String id =rs.getString(1);
+			String nombre=rs.getString(2);
+			System.out.println(id + " " + nombre);
+		}
+		QuerySelect = "select * from alumno";
+		st = conn.createStatement();
+		rs = st.executeQuery(QuerySelect);
+		
+		System.out.println("Alumnos");
+		while( rs.next()){
+			String id =rs.getString(1);
+			String nombre=rs.getString(2);
+			System.out.println(id + " " + nombre);
+		}
+		
+		
+		
+		st.close();
+		rs.close();
+		conn.close();
+	}catch (Exception e) {
+		System.out.println("Error con el insert "+e.toString());
+	}
+    }
+    
+    
+}
---
+++ EJERCICIOS/ALEJANDRO_JIMENEZ/bdd/src/main/java/com/everis/beca/BaseDeDatos/ConexionesBD.java
@@ -0,0 +1,39 @@
+package com.everis.beca.BaseDeDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConexionesBD {
+	
+	private static final String URL_DB = "jdbc:mysql://10.118.124.196:3306/beca_java?useTimezone=true&serverTimezone=UTC";
+	private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
+	private static final String USER = "beca_java";
+	private static final String PWD = "beca_java";
+	public Connection getConnection(){
+		Connection conn = null;
+		
+
+
+				try {
+						Class.forName(DRIVER_CLASS).newInstance();
+						conn= DriverManager.getConnection(URL_DB, USER, PWD);
+				} catch (InstantiationException e) {
+					System.out.println("Error obteniendo el driver" + e.toString());
+				} catch (IllegalAccessException e) {
+					System.out.println("Error obteniendo el driver" + e.toString());
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					System.out.println("Error obteniendo el driver" + e.toString());
+				} catch (SQLException e) {
+					System.out.println("Error obteniendo la conexion" + e.toString());
+				}
+
+
+		return conn;
+		}	
+}
+
+
+
