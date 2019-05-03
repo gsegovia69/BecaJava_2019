dbd91254c73998f0c16aaacc5feb4e4157d4b278
Jaime <jpg9_1hotmail.com>
2019-05-03 12:49:51 +0200

    Create App2.java

---
+++ EJERCICIOS/JAIME_PALAZON/src/mavenJaime2/App2.java
@@ -0,0 +1,41 @@
+package com.everis.jaime.mavenJaime2;
+
+
+
+
+
+public class App2 {
+	
+	public static void main( String[] args ) {
+		
+	try {
+		ListadoAlumnosA a = new ListadoAlumnosA();
+		ListadoAlumnosB b = new ListadoAlumnosB(); 
+		System.out.println("-------------------------------------------");
+		System.out.println("-------------------------------------------");
+		System.out.println("-------------------------------------------");
+		 System.out.println("METODO1");
+		 System.out.println("-------------------------------------------");
+		 System.out.println("-------------------------------------------");
+		 System.out.println("-------------------------------------------");
+		a.obtenerClase2();
+		System.out.println("-------------------------------------------");
+		System.out.println("-------------------------------------------");
+		System.out.println("-------------------------------------------");
+		 System.out.println("METODO2");
+		 System.out.println("-------------------------------------------");
+		 System.out.println("-------------------------------------------");
+		 System.out.println("-------------------------------------------");
+		b.obtenerClaseB();
+		 
+	} catch (Exception e) {
+		
+	}
+}
+	
+	
+}
+	
+	
+	
+
