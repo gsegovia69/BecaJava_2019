3a0ea706b6d58ffd5a1c35ce712d1f8703a9e66e
Ivan <isrmail87@gmail.com>
2019-05-06 17:36:41 +0200

    Volviendo a estar bien

--- EJERCICIOS/IVAN_SANCHEZ/conexionBaseDatos/src/main/java/com/everis/beca/java/conexionBaseDatos/GetConection.java
+++ EJERCICIOS/IVAN_SANCHEZ/conexionBaseDatos/src/main/java/com/everis/beca/java/conexionBaseDatos/GetConection.java
@@ -6,19 +6,18 @@ import java.sql.SQLException;
 
 public class GetConection {
 
-	private static final String URL_DB = "jdbc:mysql://10.118.124.196:3306/beca_java?useTimezone=true&serverTimezone=UTC";
+	private static final String URL_DB = "jdbc:mysql://ALC-67L2MH2:3306/beca_java?useTimezone=true&serverTimezone=UTC";
 	private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
 	private static final String USER = "beca_java";
 	private static final String PWD = "beca_java";
 
 	public Connection getConnection() {
-Connection conn = null;
+		Connection conn = null;
 		try {
 //		Carga del driver jdbc
 			Class.forName(DRIVER_CLASS).newInstance();
 //			Conexion con la base de datos.
-			conn = DriverManager.getConnection(URL_DB,USER,PWD);
-
+			conn = DriverManager.getConnection(URL_DB, USER, PWD);
 
 		} catch (ClassNotFoundException e) {
 			System.out.println("Error al conectar con el driver JDBC");
@@ -32,7 +31,6 @@ Connection conn = null;
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 
 		return conn;
 	}
