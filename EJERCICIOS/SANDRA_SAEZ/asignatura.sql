b4b38150b924a51acdab8ffd10744d233520a99e
sandra <sannnd.sa@gmail.com>
2019-05-06 16:49:45 +0200

    update sql

--- EJERCICIOS/SANDRA_SAEZ/asignatura.sql
+++ EJERCICIOS/SANDRA_SAEZ/asignatura.sql
@@ -11,14 +11,14 @@ CREATE TABLE asignatura (
    
 );
 
-INSERT INTO asignatura VALUES (1, 'Java', 1, 1);
-INSERT INTO asignatura VALUES (2, 'Sql', 2 , 1);
-INSERT INTO asignatura VALUES (3, 'Spring', 3, 1);
-INSERT INTO asignatura VALUES (4, 'Spring Jpa', 4, 1);
-INSERT INTO asignatura VALUES (5, 'Spring Core', 5, 1);
-INSERT INTO asignatura VALUES (6, 'Spring Boot', 6, 1);
-INSERT INTO asignatura VALUES (7, 'Spring Rest', 7, 1);
-INSERT INTO asignatura VALUES (8, 'Spring Mvc', 8, 1);
+INSERT INTO asignatura VALUES ('Java', 1, 1);
+INSERT INTO asignatura VALUES ('Sql', 2 , 1);
+INSERT INTO asignatura VALUES ('Spring', 3, 1);
+INSERT INTO asignatura VALUES ('Spring Jpa', 4, 1);
+INSERT INTO asignatura VALUES ('Spring Core', 5, 1);
+INSERT INTO asignatura VALUES ('Spring Boot', 6, 1);
+INSERT INTO asignatura VALUES ('Spring Rest', 7, 1);
+INSERT INTO asignatura VALUES ('Spring Mvc', 8, 1);
 
 
  
