ad42cb7ab4e771ff4a0359bdb2966d423a794050
gsegovia69 <gsegovie1969@gmail.com>
2019-05-06 17:31:05 +0200

    o

---
+++ EJERCICIOS/ALEJANDRO_OTERO/sql/SentenciasSQL.java
@@ -0,0 +1,63 @@
+package com.everis.beca.BaseDeDatos;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.naming.ReferralException;
+
+public class SentenciasSQL {
+
+	public static void main(String[] args) {
+		
+		try {
+			ConnectionDataBase connectionDatabase = new ConnectionDataBase();
+			Connection conn = connectionDatabase.getConnection();
+			Statement st = conn.createStatement();
+			
+			//String queryInsert= "insert into alumno(nombre,apellidos,email,ciudad,id_clase) values ('Alejandro','Jimenez Erades','alejandrojimenezerades@gmail.com','Aspe',1)";
+			//st.executeUpdate(queryInsert);
+			
+			ResultSet rs = null;
+			String querySelect = "Select * from alumno";
+			st = conn.createStatement();
+			rs = st.executeQuery(querySelect);
+			System.out.println("Todos los datos de alumno\n");
+			while (rs.next()) {
+						for(int i=1;i<rs.getMetaData().getColumnCount();i++) {
+							System.out.print(rs.getString(i)+" ");
+							if (i==rs.getMetaData().getColumnCount()-1) {
+								System.out.print("\n");
+							}
+						}
+					
+			}
+			
+			querySelect = "select nombre, email from alumno where nombre like '%s%' ";
+			st = conn.createStatement();
+			rs = st.executeQuery(querySelect);
+			System.out.println("\nNombre y Email de alumnos que tienen una s en el nombre\n");
+			while(rs.next()) {
+				System.out.println(rs.getString(1) + " " + rs.getString(2));
+			}
+			
+			querySelect = "select a.nombre , c.nombre from alumno a , clase c where c.id=a.id_clase and apellidos like '%a%' ";
+			st = conn.createStatement();
+			rs = st.executeQuery(querySelect);
+			System.out.println("\nNombre y Clase de alumnos que tienen una a en el apellido\n");
+			
+			while(rs.next()) {
+				System.out.println(rs.getString(1) + " " + rs.getString(2));
+			}
+	
+			st.close();
+			rs.close();
+			conn.close();		
+		} catch(Exception e) {
+			System.err.println("Error " + e.getMessage());
+			
+		}
+		
+	}
+
+}
