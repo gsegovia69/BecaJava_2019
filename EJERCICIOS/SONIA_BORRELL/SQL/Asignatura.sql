7c314c0ac99bc703408cb6ea0f54e837fd91099e
Sonia <s.borrell.p@gmail.com>
2019-05-06 17:14:31 +0200

    Update Asignatura.sql

--- EJERCICIOS/SONIA_BORRELL/SQL/Asignatura.sql
+++ EJERCICIOS/SONIA_BORRELL/SQL/Asignatura.sql
@@ -2,15 +2,16 @@ CREATE TABLE IF NOT EXISTS'Asignatura' (
     id INT not null auto-increment;
     nombre varchar,
     orden INT,
-    clase varchar,
+    clase INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY id_clase references 'clase' (id),
+    FOREIGN KEY fk_asig_clase(id_clase) references 'clase' (id),
     );
-insert asignatura values ('java', 1, 1);
-insert asignatura values ('sql', 2, 1);
-insert asignatura values ('spring', 3, 1);
-insert asignatura values ('spring jpa', 4, 1);
-insert asignatura values ('spring core', 5, 1);
-insert asignatura values ('spring boot', 6, 1);
-insert asignatura values ('spring rest', 7, 1);
-insert asignatura values ('spring mvc', 8, 1);
+
+insert into asignatura values ('java', 1, 1);
+insert into asignatura values ('sql', 2, 1);
+insert into asignatura values ('spring', 3, 1);
+insert into asignatura values ('spring jpa', 4, 1);
+insert into asignatura values ('spring core', 5, 1);
+insert into asignatura values ('spring boot', 6, 1);
+insert into asignatura values ('spring rest', 7, 1);
+insert into asignatura values ('spring mvc', 8, 1);
