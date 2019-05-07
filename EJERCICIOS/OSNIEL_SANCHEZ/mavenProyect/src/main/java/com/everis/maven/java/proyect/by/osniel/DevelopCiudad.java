7f209475712e2c961b365da63096920ba589faf5
osniel <onmymusica@gmail.com>
2019-05-07 10:12:04 +0200

    sql

---
+++ EJERCICIOS/OSNIEL_SANCHEZ/mavenProyect/src/main/java/com/everis/maven/java/proyect/by/osniel/DevelopCiudad.java
@@ -0,0 +1,24 @@
+package com.everis.maven.java.proyect.by.osniel;
+import java.util.List;
+import com.everis.maven.java.proyect.by.osniel.dto.Alumno;
+import com.everis.maven.java.proyect.by.osniel.dto.Clase;
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
+public class DevelopCiudad{
+	List<Alumno> alumno;
+	public void ordenaCiuad() {
+		this.alumno
+        .stream()
+        .filter(Alumno::getCiudadContainsA)
+        .map(Alumno::getaAlumnosFormatoFecha)
+        .forEach(System.out::println);
+	};
+	
+	public void ordenaCiuad(Clase clase) {
+		clase.getAlumnos()
+		.stream()
+		.filter(a-> a.getCiudad().toLowerCase().startsWith("a"))
+		.forEach(a->System.out.println(a.getaAlumnosFormatoFecha()));
+		
+	};
+}
