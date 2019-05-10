affcd2873db0bf9023f3711b658aa84d4e28cfe5
DaniB <dbuyolo76@gmail.com>
2019-05-10 08:22:10 +0200

    Upload Ejercicio
    Demo con Spring

---
+++ EJERCICIOS/DANIEL_BUYOLO/demo-demo/src/main/java/com/demo/prueba/DemoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDemoApplication.class, args);
+	}
+
+}
---
+++ EJERCICIOS/DANIEL_BUYOLO/demo-demo/src/test/java/com/demo/prueba/DemoApplicationTest.java
@@ -0,0 +1,5 @@
+package com.demo.prueba;
+
+public class DemoApplicationTest {
+
+}
