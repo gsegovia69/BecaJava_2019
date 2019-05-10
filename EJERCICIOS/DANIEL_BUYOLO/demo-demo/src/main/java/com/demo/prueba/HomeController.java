9f8e1ab24714f726bf49dce8d3323e5fe4d41922
DaniB <dbuyolo76@gmail.com>
2019-05-10 14:55:37 +0200

    Update 10-05-19
    Subida Ejercicio SpringBoot

--- EJERCICIOS/DANIEL_BUYOLO/demo-demo/src/main/java/com/demo/prueba/HomeController.java
+++ EJERCICIOS/DANIEL_BUYOLO/demo-demo/src/main/java/com/demo/prueba/HomeController.java
@@ -1,13 +1,25 @@
 package com.demo.prueba;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.java.Log;
+
+@Log
 @Controller
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "atrib.variable")
 public class HomeController {
 
+	private String atributo = "Hola mundo";
+	
 	@GetMapping("/")
 	public String home() {
+		log.info("Atributo: "+ atributo);
 		return "home";
 	}
 }
